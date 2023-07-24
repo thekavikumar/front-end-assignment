9a379cc0864fae77a53320c1a74f8cd258d20654
thekavikumar <kavikumarceo@gmail.com>
2023-07-24 22:42:33 +0530

    70% work done

--- app/api/create/route.ts
+++ app/api/create/route.ts
@@ -1,18 +1,33 @@
 import clientPromise from "@/lib/mongodb";
-import {  NextApiResponse } from "next";
+import { NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 
 const handler = async (req: Request, res: NextApiResponse) => {
+  function generateRandomApiKey(length: number): string {
+    const characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;:,.<>?";
+    const apiKeyArray = [];
+
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      apiKeyArray.push(characters[randomIndex]);
+    }
+
+    return apiKeyArray.join("");
+  }
+
   try {
     const client = await clientPromise;
     const db = client.db("test");
     const data = await req.json();
+    const apiKey = generateRandomApiKey(32);
     console.log(data);
     const obj = {
       app_name: data.app_name,
       app_description: data.app_description,
       app_providers: data.app_providers,
       user_email: data.user_email,
+      api_key: apiKey,
     };
     const collection = db.collection("apps");
     await collection.insertOne(obj);
